ae61eadb4258955d18e700826cee6549900ef3d3
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-02-16 17:33:32 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
- * progiming in c
- * testing betty
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
