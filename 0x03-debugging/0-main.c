ef9197ea38ae4d4f417298019af85f47549fe7f3
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-02-22 14:52:06 -0800

    test_0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,19 +10,7 @@ int main(void)
 	int i;
 
 	i = 98;
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
-
+	positive_or_negative(i);
 
 	return (0);
 }
