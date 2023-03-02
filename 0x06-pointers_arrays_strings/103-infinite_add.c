308bb48db344dafe15f9a1d5a2c7469eb2faf8b8
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-03-02 15:00:46 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,56 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * infinite_add - Adds two numbers.
+ *
+ * @n1: The first number.
+ * @n2: The second number.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
+ * Return: A pointer to the result or 0 if it can not be stored in r.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int len1 = 0, len2 = 0, i = 0, j = 0, sum = 0, carry = 0;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
+	/* Calculate the lengths of n1 and n2 */
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+
+	/* Check if the result can be stored in r */
+	if (len1 + 1 > size_r || len2 + 1 > size_r)
 		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+
+	/* Add the numbers digit by digit */
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry > 0; i--, j--)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+		carry = sum / 10;
+		r[(i > j ? i : j) + 1] = (sum % 10) + '0';
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+
+	/* Add the null terminator */
+	r[(i > j ? i : j) + 2] = '\0';
+
+	/* Reverse the string */
+	len1 = 0;
+	len2 = (i > j ? i : j) + 1;
+	while (len1 < len2)
+	{
+		sum = r[len1];
+		r[len1] = r[len2];
+		r[len2] = sum;
+		len1++;
+		len2--;
+	}
+
+	return (r);
 }
+
