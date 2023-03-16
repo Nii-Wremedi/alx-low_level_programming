64f3a48535e9cd6b11ea204b95add44c407b05e3
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-03-15 18:40:30 -0700

    correction 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,19 +4,20 @@
 
 /**
  * main - program that multiplies two numbers
- * @argc: argument count
- * @argv: arg vectors
+ * @argc: argument counter
+ * @argv: numbers to multiply
  * Return: 0 on success, 1 if two arguments not given
  */
 
 int main(int argc, char *argv[])
 {
 
-	if (argc == 3)
-
-
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	else
+	if (argc != 3)
+	{
 		printf("Error\n");
 		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
