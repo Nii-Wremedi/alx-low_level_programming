f521afe0c71637f8ff009e08367ac62b4d7cd440
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-03-20 16:45:20 -0700

    structure for dog info file - dog.h and declaration of prototype functions

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+A structure is a data type defined by the user in C - programming.
+
+Structures allow for the combination of deifferent data types in a function
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *my_dog -  typedef for struct dog
+ */
+typedef struct dog my_dog
+
+/**
+ * struct dog - struct that stores information of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*_DOG_H_*/
