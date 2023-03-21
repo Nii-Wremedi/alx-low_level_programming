a513affb867ba2ca688d3beafa255d66225553c9
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-03-20 17:13:12 -0700

    structure for dog info file - dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - data types for structure of dog info
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner's name
+ */
+
 struct dog
 {
 	char *name;
