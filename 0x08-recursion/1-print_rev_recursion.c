6a0e2a75e3a3ab5ea2f1397f7b1d4c7e0e2a9b41
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-08 18:10:57 -0700

    function that prints a string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -14,14 +14,3 @@ void _print_rev_recursion(char *s)
 	_print_rev_recursion(s + 1);
 	putchar(*s);
 }
-
-/**
- * main - Entry point:
- * Return: 0 on success
- */
-
-int main(void)
-{
-	_print_rev_recursion("\nNii Wremedi");
-	return (0);
-}
