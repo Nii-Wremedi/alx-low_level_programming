fc1a0193d21056f463b7648e16bb4f2a8c6bc193
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-12 01:48:36 -0700

    function that checks if a number is prime

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+    int length = _strlen_recursion(s);
+
+    return check_palindrome(s, 0, length - 1);
+}
+
+/**
+ * check_palindrome - Recursive helper function to check if a string is a palindrome.
+ * @s: The string to be checked.
+ * @start: The starting index.
+ * @end: The ending index.
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise.
+ */
+int check_palindrome(char *s, int start, int end)
+{
+    if (start >= end)
+        return 1;
+
+    if (s[start] != s[end])
+        return 0;
+
+    return check_palindrome(s, start + 1, end - 1);
+}
+
+/**
+ * _strlen_recursion - Calculates the length of a string recursively.
+ * @s: The string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+    if (*s == '\0')
+        return 0;
+
+    return 1 + _strlen_recursion(s + 1);
+}
+
