cd32018fb0d4c092b9663de571b8ef5123e87c38
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-12 02:29:53 -0700

    function that returns 1 if string is a pallidrome and 0 if it's not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,48 +1,54 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to check
  *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
+
 int is_palindrome(char *s)
 {
-    int length = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-    return check_palindrome(s, 0, length - 1);
+	if (len <= 1)
+		return (1);
+
+	return (check_palindrome(s, 0, len - 1));
 }
 
 /**
- * check_palindrome - Recursive helper function to check if a string is a palindrome.
- * @s: The string to be checked.
- * @start: The starting index.
- * @end: The ending index.
+ * check_palindrome - Recursively checks if a string is a palindrome
+ * @s: The string to check
+ * @start: The starting index of the substring
+ * @end: The ending index of the substring
  *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: 1 if the substring is a palindrome, 0 otherwise
  */
+
 int check_palindrome(char *s, int start, int end)
 {
-    if (start >= end)
-        return 1;
+	if (start >= end)
+		return (1);
 
-    if (s[start] != s[end])
-        return 0;
+	if (s[start] != s[end])
+		return (0);
 
-    return check_palindrome(s, start + 1, end - 1);
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
- * _strlen_recursion - Calculates the length of a string recursively.
- * @s: The string.
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string to measure
  *
- * Return: The length of the string.
+ * Return: The length of the string
  */
+
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return 0;
+	if (*s == '\0')
+		return (0);
 
-    return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
-
