31c88e8fa54c6c710fe39554fdde6c686c505e07
Nii-Wremedi <jeskingamu.ja@gmail.com>
2023-06-19 06:36:43 -0700

    structure type and function declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,9 +17,13 @@ struct dog
 	char *owner;
 };
 
-/*declaration of function prototypes*/
+typedef struct dog dog_t;
+
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif /* DOG_H */
+
